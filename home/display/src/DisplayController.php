4977246c9e57eb08457fb957be3765900cfcefcc
lenguyenquocvu <lenguyenquocvucd17tt10@gmail.com>
2019-10-12 07:07:51 +0700

    demo

---
+++ home/display/src/DisplayController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Home\Display;
+
+use App\Http\Controllers\Controller;
+
+Class DisplayController extends Controller
+{
+
+    public function index()
+    {
+		return view('display::index', array());
+	}
+
+}
---
+++ home/display/src/routes/web.php
@@ -0,0 +1,2 @@
+<?php
+Route::get('myhome', 'Home\Display\DisplayController@index');
